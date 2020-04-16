a40b9c56240065a7cfc9e1f0b2b2aedee446938f
Kokin18Kirill <62745363+Kokin18Kirill@users.noreply.github.com>
2020-04-16 14:02:20 +0700

    Add files via upload

---
+++ Шахматы/Шахматы/Class1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Шахматы
+{
+   public static class Paint
+    {
+
+        public static void Pole()
+        {
+            //Console.ForegroundColor = ConsoleColor.DarkBlue;//изменение цвета шрифта.
+           
+
+            //Console.BackgroundColor = ConsoleColor.Yellow;//изменяет фона за текстом.
+
+
+            //int k = 2;
+            //int l = -1;
+
+            //int[,] pole = new int[8, 8];
+            //for (int i = 0; i < 8; i++)
+            //{
+            //    for (int j = 0; j < 8; j++)
+            //    {
+            //        k = k + l;
+            //        if (k == 1)
+            //        {
+            //            Console.ForegroundColor = ConsoleColor.Red;
+            //            Console.BackgroundColor = ConsoleColor.Black;
+            //            Console.SetCursorPosition(j, i);
+            //            Console.WriteLine("k");
+            //            l = l * (-1);
+            //        }
+            //        else
+            //        {
+            //            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            //            Console.BackgroundColor = ConsoleColor.White;
+            //            Console.SetCursorPosition(j, i);
+            //            Console.WriteLine("C");
+            //            l = l * (-1);
+            //        }
+
+
+            //    }
+
+            //}
+        }
+    }
+}
---
+++ Шахматы/Шахматы/Logik.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Шахматы
+{
+    public static class Logik
+    {
+        public static int x, y;
+        public static int[] BX = new int[15];
+        public static int[] BY = new int[15];
+        public static int[] WX = new int[15];
+        public static int[] WY = new int[15];
+
+        public static void Figure_choice(ConsoleKeyInfo keyInfo,char[,] pole,int[,] pole2, int user)
+        {
+            char figura;
+
+            if (keyInfo.Key == ConsoleKey.RightArrow)
+            {
+                if (x + 1<=7)
+                {
+                    x++;
+                    Console.SetCursorPosition(x, y);            
+                }
+            }
+            if (keyInfo.Key == ConsoleKey.UpArrow)
+            {
+                if (y - 1 >= 0)
+                {
+                    y--;
+                    Console.SetCursorPosition(x, y);                
+                }
+            }
+            if (keyInfo.Key == ConsoleKey.LeftArrow)
+            {
+                if (x - 1 >= 0)
+                {
+                    x--;
+                    Console.SetCursorPosition(x, y);                 
+                }                    
+            }
+            if (keyInfo.Key == ConsoleKey.DownArrow)
+            {
+                if (y + 1 <=7)
+                {
+                    y++;
+                    Console.SetCursorPosition(x, y );                   
+                }                         
+            }
+            if (keyInfo.Key == ConsoleKey.Enter)
+            {
+                if ()// проверка на то кто ходит
+                    figura = pole[y, x];
+                Console.SetCursorPosition(x, y);
+                if (pole2[y,x] == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.Write(' ');
+                }
+                if (pole2[y,x]== 1)
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.Write(' ');
+                }
+            }
+            if (keyInfo.Key == ConsoleKey.Spacebar)
+            {
+                if()// проверка на то кто ходит
+            }
+        }
+    }
+}
